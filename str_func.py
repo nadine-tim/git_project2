333ed8f79f5e11b3333ad120e2eba35f1b145070
nadine-tim <nadinetimeiko@gmail.com>
2023-03-14 20:30:43 +0300

    add function return_with_first_capital_letters and docstrings

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
-def return_with_capital_letters():
+def return_with_all_capital_letters():
+    """return user input with all capital letters"""
+    user_input = input()
+    return user_input.upper()
+
+def return_with_first_capital_letters():
+    """return user input with first capital letters of words"""
     user_input = input()
     return user_input.title()
