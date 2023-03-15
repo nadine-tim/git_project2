6065231949e75d9d3dc41d782e71bc5a05b5ca6c
nadine-tim <nadinetimeiko@gmail.com>
2023-03-15 19:29:40 +0300

    add docstring into func add second func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def return_with_all_capital_letters(string):
+    """return string with all capital letters"""
     return string.upper()
+
+def return_with_first_capital_letters(string):
+    """return string with first capital letters"""
+    return string.title()
