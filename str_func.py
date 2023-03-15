2854151d59184e8b101cd553039281803f3fd703
nadine-tim <nadinetimeiko@gmail.com>
2023-03-15 19:05:05 +0300

    del str_func

--- str_func.py
+++
@@ -1,9 +0,0 @@
-def return_with_all_capital_letters():
-    """return user input with all capital letters"""
-    user_input = input()
-    return user_input.upper()
-
-def return_with_first_capital_letters():
-    """return user input with first capital lettes"""
-    user_input = input()
-    return user_input.title()
