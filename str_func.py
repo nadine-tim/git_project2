4d24e4b360f3d4ea20907938f8a5e04a7d9e5986
nadine-tim <nadinetimeiko@gmail.com>
2023-03-15 20:29:18 +0300

    rename funcs

--- str_func.py
+++ str_func.py
@@ -1,8 +1,8 @@
-def return_with_all_capital_letters(string):
+def capitalize_all_letters(string):
     """return string with all capital letters"""
     return string.upper()
 
 
-def return_with_first_capital_letters(string):
+def capitalize_first_letters(string):
     """return string with first capital letters"""
     return string.title()
